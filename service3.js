506d051f9614b509aee4c141b161291cdc85ef97
piyanatnine <56292153+piyanatnine@users.noreply.github.com>
2022-04-25 17:27:02 +0700

    change port - อาจารย์อย่าพึ่งตรวจครับ

--- service3.js
+++ service3.js
@@ -6,7 +6,7 @@ const { PrometheusExporter } = require('@opentelemetry/exporter-prometheus');
 const { MeterProvider }  = require('@opentelemetry/sdk-metrics-base');
 
 // Add your port and startServer to the Prometheus options
-const options = {port: 9464, startServer: true};
+const options = {port: 9090, startServer: true};
 const exporter = new PrometheusExporter(options);
 
 // Register the exporter
@@ -16,15 +16,15 @@ const meter = new MeterProvider({
 }).getMeter('demo-prometheus');
 
 // Now, start recording data
-const counter = meter.createCounter('count_movies', {
-  description: 'Count movies api'
+const counter = meter.createCounter('62070119_api_count', {
+  description: '62070119_api_count'
 });
 
 
 app.get('/movies', async function (req, res) {
 
   counter.add(1, { service_name: "service_3" });
-  counter.add(1, { service_name: "service_3", status: "success" });
+  counter.add(1, { service_name: "service_3", status: "success", path: "" });
   counter.add(1, { service_name: "service_3", status: "failure" });
   counter.add(1, { service_name: "service_3", status: "data not found" });
 
